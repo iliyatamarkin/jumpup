6d5a205c796939fdfa5031abdcc9953e70ee7cb8
iliya <iliya.tamarkin@gmail.com>
2021-04-16 12:42:57 +0300

    added map

---
+++ components/Map.js
@@ -0,0 +1,49 @@
+import React, { useRef, useEffect, useState } from 'react';
+import mapboxgl from 'mapbox-gl';
+import styles from './Map.module.css'
+
+mapboxgl.accessToken =
+  'pk.eyJ1IjoiaWxpeWEtdGFtYXJraW4iLCJhIjoiY2pybmx6M3E5MHU5eDQ0dGs4cTFvYjE4NSJ9.OWDUyJrhXv_wHyEvrgT_5g';
+
+const Map = () => {
+  const mapContainerRef = useRef(null);
+
+  const [lng, setLng] = useState(5);
+  const [lat, setLat] = useState(34);
+  const [zoom, setZoom] = useState(1.5);
+
+  // Initialize map when component mounts
+  useEffect(() => {
+    const map = new mapboxgl.Map({
+      container: mapContainerRef.current,
+      style: 'mapbox://styles/mapbox/streets-v11',
+      center: [lng, lat],
+      zoom: zoom
+    });
+
+    // Add navigation control (the +/- zoom buttons)
+    // map.addControl(new mapboxgl.NavigationControl(), 'top-right');
+
+    map.on('move', () => {
+      setLng(map.getCenter().lng.toFixed(4));
+      setLat(map.getCenter().lat.toFixed(4));
+      setZoom(map.getZoom().toFixed(2));
+    });
+
+    // Clean up on unmount
+    return () => map.remove();
+  }, []); // eslint-disable-line react-hooks/exhaustive-deps
+
+  return (
+    <div>
+      <div className={styles.sidebarStyle}>
+        <div>
+          Longitude: {lng} | Latitude: {lat} | Zoom: {zoom}
+        </div>
+      </div>
+      <div className={styles.mapContainer} ref={mapContainerRef} />
+    </div>
+  );
+};
+
+export default Map;
--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,17 @@
 import Head from 'next/head'
 import Header from '@components/Header'
 import Footer from '@components/Footer'
+import Map from '@components/Map'
 
 export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
-        <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
         <Header title="Welcome to my app!" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
+        <Map />
       </main>
 
       <Footer />
